b4d23dab7d41d8e0341484895245ffb0b395972a
Damara DBS. Budi Setyawan <damara.setyawan@moonlay.jakarta>
2020-01-08 13:24:33 +0700

    Merge from latest customhome

---
+++ Block/Widget/LangButton.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Moonlay\CustomHome\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class LangButton extends Template implements BlockInterface
+{
+  protected $_template = "widget/langbutton.phtml";
+}
---
+++ Block/Widget/SocMed.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Moonlay\CustomHome\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class SocMed extends Template implements BlockInterface
+{
+  protected $_template = "widget/socmed.phtml";
+}
